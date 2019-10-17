045e3a49aed73f7e789cdf744b0cf269a1ea7ffc
kharazi <vahid@kharazi.net>
2019-10-17 18:41:00 +0330

    Add validateion to form

--- djtalk/users/views.py
+++ djtalk/users/views.py
@@ -44,8 +44,41 @@ def index(request):
 #         </html>
 #     """ % user_list_string
 #     return HttpResponse(output)
+def validate_user_add_request(data):
+    if len(data['firstname']) < 3:
+        return False, 'Your firstname must be greater than 3 chars', 'firstname'
+    try:
+        int(data['grade'])
+    except ValueError:
+        return False, 'Grade must be an int', 'grade'
+    return True, ''
+
 
 def user_list(request):
+    if 'firstname' in request.GET:
+        validate = validate_user_add_request(request.GET)
+        if validate[0] == True:
+            p = Person(
+                request.GET['firstname'],
+                request.GET['lastname'],
+                int(request.GET['grade'])
+            )
+            users.append(p)
+        else:
+            return render(
+                request,
+                'list.html',
+                context={
+                    'users': users,
+                    'title': "List User ha",
+                    'test_dict': {
+                        "name": "value"
+                    },
+                    'has_error': True,
+                    'error': validate[1],
+                    'field': validate[2]
+                }
+            )
     return render(
         request,
         'list.html',
